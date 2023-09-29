90c7833ab885c5eec5532477c4e2b025c77540cd
Pangkeek <supawatkhaithong@gmail.com>
2023-09-30 00:19:54 +0700

    Initial commit

---
+++ qr.py
@@ -0,0 +1,28 @@
+import qrcode
+import tkinter.messagebox
+import tkinter as tk
+
+def makeqr():
+    try :
+        img = qrcode.make(text_input)
+        type(img)
+        img.save("myqr.png")
+        tkinter.messagebox.showinfo('Done','Qr Succesfully downloaded in this folder :D')
+    except:
+        tkinter.messagebox.showinfo('something worng :(')
+            
+
+myapp=tk.Tk()
+myapp.title('qrcodemaker')
+myapp.minsize(width=150,height=100)
+title_label =tk.Label(text="Your Data", fg='red', font=20)
+title_label.pack()
+
+text_input=tk.Entry()
+text_input.pack()
+
+ok_button=tk.Button(text='Generate QRcode',command=makeqr, fg='green')
+ok_button.pack()
+
+
+myapp.mainloop()
